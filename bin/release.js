7e6f09ee46df02264559914cef920bae62e2f8ab
Enijar <TheEnijar@gmail.com>
2020-05-17 20:35:00 +0100

    fix: release script

--- bin/release.js
+++ bin/release.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
+const path = require("path");
 const { execSync } = require("child_process");
 const { version } = require("../package");
 
+const BASE_PATH = path.resolve(__dirname, "..");
+
+// Commit package.json change
+execSync(`git --git-dir ${BASE_PATH} add package.json`, { stdio: "inherit" });
+
 // Create release
-execSync(`git tag -a v${version} "${version}"`, { stdio: "inherit" });
-execSync(`git push --tags`, { stdio: "inherit" });
+execSync(`git --git-dir ${BASE_PATH} tag -a v${version} "${version}"`, { stdio: "inherit" });
+execSync(`git --git-dir ${BASE_PATH} push --tags`, { stdio: "inherit" });
