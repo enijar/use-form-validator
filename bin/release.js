95dacf4d04a8d732f48fff3783f4caa61e158a17
Enijar <TheEnijar@gmail.com>
2020-05-17 20:36:24 +0100

    fix: release script --git-dir flag

--- bin/release.js
+++ bin/release.js
@@ -6,8 +6,8 @@ const { version } = require("../package");
 const BASE_PATH = path.resolve(__dirname, "..");
 
 // Commit package.json change
-execSync(`git --git-dir ${BASE_PATH} add package.json`, { stdio: "inherit" });
+execSync(`git -c ${BASE_PATH} add package.json`, { stdio: "inherit" });
 
 // Create release
-execSync(`git --git-dir ${BASE_PATH} tag -a v${version} "${version}"`, { stdio: "inherit" });
-execSync(`git --git-dir ${BASE_PATH} push --tags`, { stdio: "inherit" });
+execSync(`git -c ${BASE_PATH} tag -a v${version} "${version}"`, { stdio: "inherit" });
+execSync(`git -c ${BASE_PATH} push --tags`, { stdio: "inherit" });
